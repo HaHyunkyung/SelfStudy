5fb10118373bb655513c8314f4bd70e38899a75b
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-07-23 22:42:07 +0900

    삼각 달팽이

---
+++ src/day0723.java
@@ -0,0 +1,39 @@
+
+public class day0723 {
+
+	public static int[] main(String[] args) {
+		// TODO Auto-generated method stub
+			int n=4;
+		   int[] answer = new int[(n*(n+1))/2];
+	        int[][] matrix = new int[n][n];
+	        
+	        int x=-1, y=0;
+	        int num = 1;
+	        
+	        for(int i=0; i<n; i++){
+	            for(int j=i; j<n; j++){
+	                if(i%3==0){
+	                    x++;
+	                }else if(i%3==1){
+	                    y++;
+	                }else if(i%3==2){
+	                    x--;
+	                    y--;
+	                }
+	                matrix[x][y] = num++;
+	            }
+	        }
+	        int k=0;
+	        for(int i=0; i<n; i++){
+	            for(int j=0; j<n; j++){
+	                if(matrix[i][j] == 0){
+	                     break;
+	                }
+	                 answer[k++] = matrix[i][j];
+	            }
+	        }
+	        
+	        return answer;
+	}
+
+}
