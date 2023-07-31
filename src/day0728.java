f8c04dfacb52a2606002501f9eaee3c0b6707a31
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-07-31 16:25:38 +0900

    최대값과 최솟값

--- src/day0728.java
+++ src/day0728.java
@@ -1,9 +1,17 @@
-
+import java.util.*;
 public class day0728 {
 
-	public static void main(String[] args) {
+	public static String main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s = "1 2 3 4";
+		 String answer = "";
+	        ArrayList<Integer> math = new ArrayList<Integer>();
+	        String[] str = s.split(" ");
+	        for(int i=0;i<str.length;i++){
+	            math.add(Integer.parseInt(str[i]));
+	        }
+	        answer = Collections.min(math)+ " "+ Collections.max(math);
+	        return answer;
 	}
 
 }
