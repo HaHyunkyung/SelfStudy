08f292fadd97b8674659feab79962f6534c2183e
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-02 17:24:14 +0900

    최솟값 만들기

--- src/day0801.java
+++ src/day0801.java
@@ -1,9 +1,25 @@
+import java.util.*;
 
 public class day0801 {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		// TODO Auto-generated method stub
+		int answer = 0;
+		int[] A = {1,4,2};
+		int[] B = {5,4,4};
+        Integer[] b = new Integer[B.length];
+        
+        for(int i=0; i<b.length; i++){
+            b[i]=B[i];
+        }
+        Arrays.sort(A);
+        Arrays.sort(b,Collections.reverseOrder());
+        
+        for(int i=0; i<A.length; i++){
+            answer += A[i]*b[i];
+        }
 
+        return answer;
 	}
 
 }
