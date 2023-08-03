10e62aea96e8746ffd09e19a47affd1ab5b418fb
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-04 00:29:50 +0900

    다음 큰 숫자

--- src/day0803.java
+++ src/day0803.java
@@ -1,9 +1,35 @@
 
 public class day0803 {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		// TODO Auto-generated method stub
-
+		  int answer = 0;
+		  int n = 15;
+	        String bs = Integer.toBinaryString(n);
+	        char[] bsArray = bs.toCharArray();
+	        int bsnum=0;
+	        
+	        for(int i=0; i<bsArray.length; i++){
+	            if(bsArray[i]=='1')
+	                bsnum++;
+	        }
+	        int num =n;
+	        while(true){
+	            num++;
+	            String upgrade = Integer.toBinaryString(num);
+	            char[] upgradeArray = upgrade.toCharArray();
+	            int upnum=0;
+	             for(int i=0; i<upgradeArray.length; i++){
+	                if(upgradeArray[i]=='1')
+	                    upnum++;
+	                 
+	        }
+	            if(upnum == bsnum){
+	                answer = num;
+	                break;
+	            }
+	        }
+	        return answer;
 	}
 
 }
