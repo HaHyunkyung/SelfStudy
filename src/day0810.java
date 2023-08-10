1c1b584bbe0acb8b6686f252f04731b8c713bbfa
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-11 01:06:03 +0900

    의상

--- src/day0810.java
+++ src/day0810.java
@@ -1,9 +1,22 @@
-
+import java.util.*;
 public class day0810 {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HashMap<String, Integer> map = new HashMap<>();
+		String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        for (String[] clothe : clothes) {
+            String type = clothe[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        Iterator<Integer> it = map.values().iterator();
+        int answer = 1;
+        
+        while(it.hasNext())
+            answer *= it.next().intValue() + 1;
+        
+        return answer - 1;
 	}
 
 }
