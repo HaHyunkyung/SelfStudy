e240fb21cebbc2509de224c3060069b77f4068f1
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-15 02:31:59 +0900

    전화번호 목록

--- src/day0813.java
+++ src/day0813.java
@@ -1,9 +1,21 @@
-
+import java.util.*;
 public class day0813 {
 
-	public static void main(String[] args) {
+	public static boolean main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] phoneBook = {"119", "97674223", "1195524421"};
+		 Map<String, Integer> map = new HashMap<>();
 
+	     
+	        for (int i = 0; i < phoneBook.length; i++) 
+	            map.put(phoneBook[i], i);
+	        
+	        for (int i = 0; i < phoneBook.length; i++)
+	            for (int j = 0; j < phoneBook[i].length(); j++)
+	                if (map.containsKey(phoneBook[i].substring(0, j)))
+	                    return false;
+
+	        return true;
 	}
 
 }
