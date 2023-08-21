f3a1c875dc5808b419878974aca457e56de1f416
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-21 22:14:28 +0900

    행렬 테두리 회전(미완성)

--- src/day0820.java
+++ src/day0820.java
@@ -1,9 +1,34 @@
+import java.util.*;
 
 public class day0820 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int rows = 6;
+		int columns = 6;
+		int[][] queries = {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
+		int[] answer = new int[3];
+		int[][] num = new int[rows][columns];
+		int nums = 1;
+		
+		 for(int i=0; i<rows; i++){
+	            for(int j=0;j<columns;j++){
+	                num[i][j] = nums;
+	                nums++;
+	            }
+	        }
+		 for(int i=0; i<queries.length; i++) {
+			 int[][] circle = new int[(queries[i][2]-queries[i][0])+1][(queries[i][3]-queries[i][1])+1];
+			 for(int j=0; j<circle.length; j++) {
+				 for(int k=0; k<circle[j].length; k++) { 
+					 circle[j][k] = 1;
+					 System.out.print(circle[j][k]);
+				 }
+				 System.out.println();
+			 }
+		 }
+		 
+		
 	}
 
 }
