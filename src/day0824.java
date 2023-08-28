d553176b3cd201fba6d1bb977129d1654e8aeb6f
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-29 00:50:00 +0900

    124나라의 숫자

--- src/day0824.java
+++ src/day0824.java
@@ -1,9 +1,24 @@
 
 public class day0824 {
 
-	public static void main(String[] args) {
+	public static String main(String[] args) {
 		// TODO Auto-generated method stub
-		
+				  int n = 125;
+			      String[] numbers = {"4", "1", "2"};
+			      String answer = "";
+			      
+			      int num = n;
+			      
+			      while(num > 0){
+			          int remainder = num % 3;
+			          num /= 3;
+			          
+			          if(remainder == 0) num--;
+			          
+			          answer = numbers[remainder] + answer;
+			      }
+			      
+			      return answer;
 	}
 
 }
