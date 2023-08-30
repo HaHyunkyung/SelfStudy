cc86c179a3cb29c41c52ffc96c021f50961de7b8
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-08-31 02:07:48 +0900

    롤케이크 자르기

--- src/day0825.java
+++ src/day0825.java
@@ -1,9 +1,33 @@
+import java.util.*;
 
 public class day0825 {
-
+	
+	public static void cutting(int[] topping, int size) {
+		int answer = 0;
+		HashSet<Integer> first = new HashSet<>();
+		HashMap<Integer, Integer> second = new HashMap<>();
+		
+		first.add(topping[0]);
+		for(int i=1; i<size; i++) {
+			second.put(topping[i], second.get(topping[i])-1);
+			if(second.get(topping[i])==0) {
+				second.remove(topping[i]);
+			}
+			if(first.size()==second.size())
+				answer++;
+			
+		}
+		System.out.println(answer);
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] topping = {1,2,1,3,1,4,1,2};
+		int size = topping.length;
+		
+		cutting(topping,size);
+		
 	}
 
 }
