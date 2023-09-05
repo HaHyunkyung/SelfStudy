1ee857317e29c7f8658a0477c77ac8f82f2f57e1
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-09-05 15:09:56 +0900

    타겟 넘버(DFS)

--- src/day0903.java
+++ src/day0903.java
@@ -1,9 +1,24 @@
 
 public class day0903 {
-
+static int count = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] numbers = {1,1,1,1,1};
+		int target = 3;
+		int answer = 0;
+		dfs(numbers,target, 0,0);
+		answer = count;
+		System.out.println(answer);
+	}
 
+	private static void dfs(int[] numbers, int target, int idx, int sum) {
+		// TODO Auto-generated method stub
+		if(idx == numbers.length) {
+			if(sum == target) count++;
+		}else {
+			dfs(numbers, target, idx+1, sum+numbers[idx]);
+			dfs(numbers, target, idx+1, sum-numbers[idx]);
+		}
 	}
 
 }
