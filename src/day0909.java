16940a313a93217f2d52e90ca4d4ce277c4e5c0e
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-09-11 20:46:42 +0900

    줄 서는 방법

--- src/day0909.java
+++ src/day0909.java
@@ -1,9 +1,27 @@
+import java.util.*;
 
 public class day0909 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] n = {1,2,3};
+		int[] answer = new int[n.length];
+		List<Integer> list = new ArrayList<>();
+		int k=5;
+		
+		long f = 1;
+		for(int i=1; i<=n.length; i++) {
+			list.add(i);
+			f *=i;
+		}
+		k--;
+		int idx = 0;
+		while(idx<n.length) {
+			f /= n.length -idx;
+			answer[idx++] = list.remove((int)(k/f));
+			k%=f;
+		}
+		System.out.println(answer);
 	}
 
 }
