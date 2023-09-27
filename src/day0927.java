36ea08029a2bb3bff36ee10c5281b90083d357de
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-09-28 00:20:02 +0900

    퇴사.ver1

--- src/day0927.java
+++ src/day0927.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class day0927 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int day[] = new int[num];
+		int pay[] = new int[num];
+		for(int i=0; i<num; i++) {
+			String schedule = br.readLine();
+			day[i] = Integer.parseInt(schedule.split(" ")[0]);
+			pay[i] = Integer.parseInt(schedule.split(" ")[1]);
+		}
+		
 	}
 
 }
