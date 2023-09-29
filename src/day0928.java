c14453fc3e289788f4513c5bbfbfc22e597c13d5
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-09-29 23:40:53 +0900

    day0928

---
+++ src/day0928.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class day0928 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		
+	}
+
+}
