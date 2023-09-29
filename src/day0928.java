e7abad621c734c3fc3eb2de252e41b133505b67c
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-09-29 23:49:03 +0900

    2xn 타일링

--- src/day0928.java
+++ src/day0928.java
@@ -11,7 +11,19 @@ public class day0928 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
 		
+		int dp[] = new int[num+1];
 		
+		for(int i=1; i<=num; i++) {
+			if(i==1) {
+				dp[i]=1;
+			}
+			else if(i==2) {
+				dp[i] = 2;
+			}else {
+				dp[i]=(dp[i-2]+dp[i-1])%10007;
+			}
+		}
+		System.out.println(dp[num]);
 	}
 
 }
