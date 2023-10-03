7b6235fefce9f3202a09485f644fdd802da2603d
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-10-04 01:51:42 +0900

    day1004

--- src/day1003.java
+++ src/day1003.java
@@ -1,10 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class day1003 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] xy = new int[6];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i=0; i<6; i++) {
+			xy[i] = Integer.parseInt(st.nextToken());
+			System.out.println(xy[i]);
+		}
 	}
 
 }
