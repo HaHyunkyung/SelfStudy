bdf769ed2363941c24c0bfd264d30c4ddb3f8992
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-10-05 21:49:18 +0900

    좌표 정렬하기

--- src/day1005_2.java
+++ src/day1005_2.java
@@ -8,15 +8,30 @@ public class day1005_2 {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		int[] x = new int[N];
-		int[] y = new int[N];
+		int[][] arr = new int[N][2];
 		for(int i =0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			x[i] = Integer.parseInt(st.nextToken());
-			y[i] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			}
+		
+		Arrays.sort(arr, (e1,e2)-> {
+			if(e1[0] == e2[0]) {
+				return e1[1]-e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<N; i++) {
+			sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
 		}
+		System.out.println(sb);
+		
 	}
 
 }
